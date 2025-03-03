be6cdfffca34b09cd4050a9be3b29ee2626ea929
jijamjk033 <jijamjk333@gmail.com>
2025-03-03 18:43:46 +0530

    updated npx env builder in frontend

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -8,7 +8,6 @@ class UserController {
         try {
             const user = await userService.loginWithGoogle(req.body);
             res.status(StatusCodes.OK).json(createSuccessResponse(user, "Login successful"));
-            console.log("User logged in:", user);
         } catch (error) {
             console.error("Google login error:", error);
             res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(createErrorResponse("Internal Server Error"));
